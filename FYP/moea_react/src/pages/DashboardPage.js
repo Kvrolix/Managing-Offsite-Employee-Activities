00017216a13988780d0c1f707741df20de88e986
Kvrolix <filipfilip306@gmail.com>
2024-03-29 13:42:31 +0000

    works but with issues
    The issue is that the page is relaoded it loses the state and all the informations

--- FYP/moea_react/src/pages/DashboardPage.js
+++ FYP/moea_react/src/pages/DashboardPage.js
@@ -1,48 +1,70 @@
 // console.log('Dashboard Page');
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import supabase from '../config/supabaseClient';
 
+import TestingUserContext from './TestingUserContext.js';
+import { UserDataProvider } from '../context/UserDataContext'; // Adjust the path to your UserDataContext file
+// Styles
 import SideNavigationBar from '../components/application/SideNaviagtionBar.js';
 import DashboardOptions from '../components/application/DashboardOptions.js';
 import SideNavigationBarCSS from '../components/application/SideNavigationBar.module.css';
+
 const DashboardPage = () => {
-	const [user, setUser] = useState({});
-	const navigate = useNavigate();
-
 	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-	// Function to toggle sidebar state
+	//  Function to toggle sidebar state
 	const toggleSidebar = () => {
 		setIsSidebarOpen(!isSidebarOpen);
 	};
 
-	// TODO from here all the data will be shared to other places?
-	useEffect(() => {
-		async function getUserData() {
-			await supabase.auth.getUser().then((value) => {
-				//  value.data.user
-				if (value.data?.user) {
-					console.log(value.data.user);
-					setUser(value.data.user);
-				}
-			});
-		}
-		getUserData();
-	}, []);
+	// const [user, setUser] = useState({});
+	// const navigate = useNavigate();
+	//
+
+	// // TODO from here all the data will be shared to other places?
+	// useEffect(() => {
+	// 	async function getUserData() {
+	// 		await supabase.auth.getUser().then((value) => {
+	// 			//  value.data.user
+	// 			if (value.data?.user) {
+	// 				console.log(value.data.user);
+	// 				setUser(value.data.user);
+	// 			}
+	// 		});
+	// 	}
+	// 	getUserData();
+	// }, []);
+	// const [fetchError, setFetchError] = useState(null);
+	// const [users, setUsers] = useState('');
+
+	// useEffect(() => {
+	// 	const fetchUsers = async () => {
+	// 		const { data, error } = await supabase.from('users2').select();
+	// 		if (error) {
+	// 			setFetchError('Could not fetch users');
+	// 			setUsers(null);
+	// 		}
+	// 		console.log(error);
+	// 		if (data) {
+	// 			console.log(data);
+	// 			setUsers(data);
+	// 			setFetchError(null);
+	// 		}
+	// 	};
+	// 	fetchUsers();
+	// }, []);
 
 	return (
 		<>
-			<SideNavigationBar
-				isSidebarOpen={isSidebarOpen}
-				toggleSidebar={toggleSidebar}
-			/>
-
-			<DashboardOptions isSidebarOpen={isSidebarOpen} />
-
-			{/* Map */}
-			{/* Calendar */}
+			<UserDataProvider>
+				<SideNavigationBar
+					isSidebarOpen={isSidebarOpen}
+					toggleSidebar={toggleSidebar}
+				/>
+				<DashboardOptions isSidebarOpen={isSidebarOpen} />
+				<TestingUserContext />
+			</UserDataProvider>
 		</>
 	);
 };
